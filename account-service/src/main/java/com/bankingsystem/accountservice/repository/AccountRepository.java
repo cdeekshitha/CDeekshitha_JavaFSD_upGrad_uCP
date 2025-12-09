25ec6d40b13904356a1d108c54e81ff2d2e22c9d
deekshitha <deekshitha.chennagani@gmail.com>
2025-12-09 15:41:04 +0530

    Delete all files

--- account-service/src/main/java/com/bankingsystem/accountservice/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankingsystem.accountservice.repository;
-
-import com.bankingsystem.accountservice.model.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends MongoRepository<Account, String> {
-}
